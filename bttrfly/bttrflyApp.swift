471186f904e3af1e62cc0f8cffe9973ce4a67a34
williamjung <williamjung0130@gmail.com>
2025-04-27 13:58:03 +0900

    Initial Commit

---
+++ bttrfly/bttrflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bttrflyApp.swift
+//  bttrfly
+//
+//  Created by William Jung on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct bttrflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
